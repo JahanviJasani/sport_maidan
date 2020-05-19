11439f865b3d9618b199d881fb9b585bab5e1723
JahanviJasani <jasanijahanvi@gmail.com>
2020-05-19 15:31:47 +0530

    My first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+
+	$('.hero_carousel').owlCarousel({
+    autoplay: false,
+    loop: true,
+    dots: true,
+    nav: false,
+    autoplayTimeout: 7000,
+    autoplaySpeed: 7000,
+    items: 1
+  }); 
+
+});
