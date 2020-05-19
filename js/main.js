9718db9196b1b37519fd5e688e5a8ed98d0e15e5
JahanviJasani <jasanijahanvi@gmail.com>
2020-05-19 18:27:35 +0530

    Added slide 02

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@ jQuery(document).ready(function ($) {
     autoplay: false,
     loop: true,
     dots: true,
+    dotsContainer: '#custom_dots',
     nav: false,
     autoplayTimeout: 7000,
-    autoplaySpeed: 7000,
+    autoplaySpeed: 2000,
     items: 1
   }); 
+
+  $('.owl-dot').click(function () {
+	  $('.hero_carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+	});
 
 });
